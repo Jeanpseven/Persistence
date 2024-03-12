7a75a21bf9e441f1cb45aca5dc78a923daf12e25
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-03-12 08:50:16 -0300

    Update code2.sh

--- code2.sh
+++ code2.sh
@@ -1,1 +1,19 @@
-sudo dd if=/dev/zero of=/media/$(lsblk -o MOUNTPOINT,NAME | grep '/media' | awk '{print $1}')/persistent.img bs=1M count=2048 && sudo mkfs.ext4 /media/$(lsblk -o MOUNTPOINT,NAME | grep '/media' | awk '{print $1}')/persistent.img && sudo mkdir /mnt/persistent && sudo mount /media/$(lsblk -o MOUNTPOINT,NAME | grep '/media' | awk '{print $1}')/persistent.img /mnt/persistent && sudo sed -i "s/linux/linux persistent persistence=LABEL=$(lsblk -o MOUNTPOINT,LABEL | grep '/media' | awk '{print $2}')/" /boot/grub/grub.cfg
+#!/bin/bash
+
+# Verifica se o arquivo grub.cfg existe
+if [ ! -f "/boot/grub/grub.cfg" ]; then
+    echo "Arquivo grub.cfg não encontrado. Baixando..."
+    
+    # Baixa o arquivo de configuração de exemplo do Kali Linux
+    sudo wget -O /boot/grub/grub.cfg https://gitlab.com/kalilinux/packages/grub2/-/raw/kali/master/debian/config/normal
+
+    # Cria o arquivo de persistência
+    sudo dd if=/dev/zero of=/media/$(lsblk -o MOUNTPOINT,NAME | grep '/media' | awk '{print $1}')/persistent.img bs=1M count=2048 && sudo mkfs.ext4 /media/$(lsblk -o MOUNTPOINT,NAME | grep '/media' | awk '{print $1}')/persistent.img && sudo mkdir /mnt/persistent && sudo mount /media/$(lsblk -o MOUNTPOINT,NAME | grep '/media' | awk '{print $1}')/persistent.img /mnt/persistent
+
+    # Adiciona a opção de persistência ao arquivo de configuração
+    sudo sed -i "s/linux/linux persistent persistence=LABEL=$(lsblk -o MOUNTPOINT,LABEL | grep '/media' | awk '{print $2}')/" /boot/grub/grub.cfg
+
+    echo "Arquivo grub.cfg configurado com sucesso."
+else
+    echo "Arquivo grub.cfg já existe. Nenhuma ação necessária."
+fi
